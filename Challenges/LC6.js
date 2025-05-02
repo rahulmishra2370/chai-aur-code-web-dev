25ab128ec820cf39828395a4168aa8d4de2cbe30
RAHUL MISHRA <rahulmishra2370@gmail.com>
2025-05-02 11:34:12 +0530

    working days problem done

---
+++ Challenges/LC6.js
@@ -0,0 +1,33 @@
+// You just need to implement the workingDays function
+function workingDays(days) {
+  // your code here
+  let totalworkingdays = 0;
+  for (let i = 0; i < days.length; i++) {
+    if (days[i] == "Sunday" || days[i] == "Saturday") {
+      totalworkingdays = totalworkingdays;
+    } else {
+      totalworkingdays++;
+    }
+  }
+  return totalworkingdays;
+}
+
+// Please don't remove the code below
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (input) => {
+  // Parse input (expected to be a JSON string array of days)
+  const days = JSON.parse(input);
+
+  // Call our function
+  const result = workingDays(days);
+
+  // Output the result as a JSON string
+  process.stdout.write(JSON.stringify(result));
+});
+// Please don't remove the code above
