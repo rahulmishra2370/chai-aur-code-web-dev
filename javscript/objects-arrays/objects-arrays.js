2e6fb0f2e200813b94df8476e67a5f8c8d73f579
RAHUL MISHRA <rahulmishra2370@gmail.com>
2025-05-09 16:35:58 +0530

    going for camp now

--- javscript/objects-arrays/objects-arrays.js
+++ javscript/objects-arrays/objects-arrays.js
@@ -124,7 +124,7 @@ console.log("Filtered array:", evenIndices); // [10, 30, 50]
 let numbers2 = [1, 2, 3, 4];
 
 // Filter numbers greater than 10
-let greaterThan10 = numbers2.filter((num) => num > 10);
+let greaterThan10 = numbers2.filter((num) => num > 10); // how's the josh
 
 console.log("Filtered array:", greaterThan10); // []
 // reduce
