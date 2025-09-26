0281098b4cefa18205944879d40fc5fa8e2dc0f3
RAHUL MISHRA <rahulmishra2370@gmail.com>
2025-09-26 12:29:20 +0530

    done with all the challenges of arrays

---
+++ Challenges/AC10.js
@@ -0,0 +1,26 @@
+// You just need to implement the countMovies function
+function countMovies(movieList) {
+  // Return the number of movies in the movieList
+  return movieList.length;
+}
+
+// Please don't remove the code below
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (input) => {
+  // Parse input (expected to be JSON string format),
+  // which should contain movieList
+  const { movieList } = JSON.parse(input);
+
+  // Call our function
+  const result = countMovies(movieList);
+
+  // Output the result as a JSON string
+  process.stdout.write(JSON.stringify(result));
+});
+// Please don't remove the code above
