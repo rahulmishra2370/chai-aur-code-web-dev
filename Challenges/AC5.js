eef2fc6ca4cac84a31c07ecd5f995b8a75584280
RAHUL MISHRA <rahulmishra2370@gmail.com>
2025-09-26 11:44:59 +0530

    challenge 5 done

---
+++ Challenges/AC5.js
@@ -0,0 +1,27 @@
+// You just need to implement the sortBooks function
+function sortBooks(books) {
+  // Sort the books alphabetically and return the updated list
+  books.sort();
+  return books;
+}
+
+// Please don't remove the code below
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (input) => {
+  // Parse input (expected to be JSON string format),
+  // which should contain books
+  const { books } = JSON.parse(input);
+
+  // Call our function
+  const result = sortBooks(books);
+
+  // Output the result as a JSON string
+  process.stdout.write(JSON.stringify(result));
+});
+// Please don't remove the code above
