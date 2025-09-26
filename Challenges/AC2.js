b68d617279eeb9de2356537166d2e7b7b4e49e49
RAHUL MISHRA <rahulmishra2370@gmail.com>
2025-09-26 11:36:11 +0530

    challenge 2 done

---
+++ Challenges/AC2.js
@@ -0,0 +1,28 @@
+// You just need to implement the eatCandy function
+function eatCandy(candyJar) {
+  // Remove the last candy from the jar and return the updated jar
+
+  candyJar.pop();
+  return candyJar;
+}
+
+// Please don't remove the code below
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", (input) => {
+  // Parse input (expected to be JSON string format),
+  // which should contain candyJar
+  const { candyJar } = JSON.parse(input);
+
+  // Call our function
+  const result = eatCandy(candyJar);
+
+  // Output the result as a JSON string
+  process.stdout.write(JSON.stringify(result));
+});
+// Please don't remove the code above
